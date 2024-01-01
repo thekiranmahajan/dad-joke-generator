9d2da46bda3175959053a01bdf900b3c2c217144
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-01 17:12:10 +0530

    JavaScript project 1/12 done ✅

---
+++ index.js
@@ -0,0 +1,35 @@
+const jokeElement = document.getElementById("joke");
+const btn = document.querySelector(".btn");
+const jokeWrapper = document.getElementById("jokeWrapper");
+const loader = document.getElementById("loader");
+const api = "238YFDFM830qHpo7wMJT9Q==5P90c45LkLGt2m4U";
+const url = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": api,
+  },
+};
+
+btn.addEventListener("click", async () => {
+  btn.disabled = true;
+  loader.style.display = "block";
+  jokeWrapper.style.display = "none";
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    jokeElement.innerHTML = data[0].joke;
+    loader.style.display = "none";
+    jokeWrapper.style.display = "block";
+    jokeWrapper.style.display = "flex";
+    btn.disabled = false;
+  } catch (error) {
+    loader.style.display = "none";
+    jokeElement.innerHTML = "Something went wrong. Please try again";
+    jokeWrapper.style.display = "block";
+    jokeWrapper.style.display = "flex";
+    btn.disabled = false;
+  }
+});
